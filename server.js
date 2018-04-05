e6567d5b50c5965b484354f121edc6a206465cdb
Enea <enealila@outlook.com>
2018-04-05 13:37:22 +0200

    Setup start script

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express=require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
+
 var app= express();
+
 hbs.registerPartials(__dirname+'/views/partials');
 
 app.set('view engine','hbs');
@@ -59,5 +62,6 @@ app.get('/home',(req,res)=>{
 
 
 
-app.listen(3000);
-console.log('Server is on port 3000 ');
+app.listen(port,()=>{
+console.log(`Server is on port ${port} `);
+});
